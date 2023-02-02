7d24ae9f360387d288898b0bb8989b9c612c4348
geetanjali chawla <geetc1919@gmail.com>
2023-02-02 17:29:59 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {   fontFamily: {
+    noto:"'Noto Sans', sans-serif",
+  ginto: "'Erica One', cursive;" },},
+
+  colors:{
+    offwhite:"#f6f6f6",
+    white:"#fff",
+    black: "#000"
+  }
+  },
+
+  plugins: [],
+}
