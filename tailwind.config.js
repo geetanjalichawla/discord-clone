b6210164063228bcd9279ffd5d1f35d7176cf14a
GEETANJALI CHAWLA <80254854+geetanjalichawla@users.noreply.github.com>
2023-03-27 17:11:29 +0530

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,16 +2,12 @@
 module.exports = {
   content: ["*"],
   theme: {
-    extend: {   fontFamily: {
-    noto:"'Noto Sans', sans-serif",
-  ginto: "'Erica One', cursive;" },},
-
-  colors:{
-    offwhite:"#f6f6f6",
-    white:"#fff",
-    black: "#000"
-  }
+    extend: {},
   },
-
   plugins: [],
+  fontFamily:{
+    Whitney:["Whitney"],
+    Ginto:["Ginto"],
+    ggSans:["ggSans"],
+  },
 }
